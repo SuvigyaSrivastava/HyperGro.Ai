a90732ee24e3f8f0ff3121962f0966761bea0221
Suvigya <suvigyasrvstva@gmail.com>
2025-06-01 01:10:34 +0530

    Used upstash instead of docker

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,3 +1,7 @@
-import Redis from 'ioredis';
+import { createClient } from 'redis';
 
-export const redis = new Redis(process.env.REDIS_URL!);
+export const redis = createClient({
+  url: process.env.REDIS_URL
+});
+
+redis.connect().catch(console.error);
